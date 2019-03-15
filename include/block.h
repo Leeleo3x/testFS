6a7e0f06a90ab9c4f5f4cfff86c27a8dfed338c8
Leeleo3x <leeleo3x@gmail.com>
2019-03-14 20:15:42 -0400

    update.

--- include/block.h
+++ include/block.h
@@ -3,9 +3,8 @@
 #include "super.h"
 
 
-
 void write_blocks(struct super_block *sb, char *blocks, int start, int nr);
-void zero_blocks(struct super_block *sb, int start, int nr);
 void read_blocks(struct super_block *sb, char *blocks, int start, int nr);
+void zero_blocks(struct super_block *sb, int start, int nr);
 
 #endif /* _BLOCK_H */
